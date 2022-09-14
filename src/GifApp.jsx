6e0147be345bae3c4bcfd1d6c707e1f805088933
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2022-09-14 14:33:33 -0400

    #75 - Agregando nueva categoría

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -3,17 +3,26 @@ import { useState } from "react"
 
 export const GifApp = () => {
 
-    const [categories, setCategories] = useState(['Dragon Ball Z', 'One Piece'])
-    console.log(categories);
+    const array = ['Dragon Ball Z', 'One Piece'];
+    const [categories, setCategories] = useState(array)
 
+    const newCate = 'The Evil Within'
+    
+    const onAddCategory = () => {
+        setCategories([newCate, ...categories ]);
+        console.log(array); 
+    }
+    
+    console.log(categories);
     return (
         <>
             <h1>GifApp</h1>
-
+            <button onClick={onAddCategory}>Agregar</button>
             <ol>
-                {categories.map(category => {
-                    return <li key={category}>{category}</li>
-                })}
+                {
+                    categories.map(category => {
+                        return <li key={category}>{category}</li>
+                    })}
             </ol>
         </>
     )
