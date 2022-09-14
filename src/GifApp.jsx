f460c1fbd7300264c245f84f403de373284d48d5
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2022-09-14 12:39:34 -0400

    #74 - Creando lista de categorías

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react"
 
 
 export const GifApp = () => {
+
+    const [categories, setCategories] = useState(['Dragon Ball Z', 'One Piece'])
+    console.log(categories);
+
     return (
         <>
             <h1>GifApp</h1>
+
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })}
+            </ol>
         </>
     )
 }
